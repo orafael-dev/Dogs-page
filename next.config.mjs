c8f45e826a30ae7f1ad1b38fea409a0aa1fa5e62
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-31 07:25:03 -0300

    chore: First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dogsapi.origamid.dev",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
