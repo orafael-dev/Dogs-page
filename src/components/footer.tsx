545138288db91feb1954eabb653c2ebf24732f4f
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-31 08:08:44 -0300

    feat: create header and footer components

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <Image src="/assets/dogs-footer.svg" alt="Dogs" width={28} height={22} />
+      <p>Todos os direitos reservados.</p>
+    </footer>
+  );
+}
