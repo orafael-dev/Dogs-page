fe9029a5521981be26ea5e2766d424e9db0c0f8c
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-07 16:13:46 -0300

    feat: crete photo-content component and get/delete actions

---
+++ src/actions/photo-delete.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { PHOTO_DELETE } from "@/app/functions/api";
+import apiError from "@/app/functions/api-error";
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function photoDelete(id: string) {
+  const token = cookies().get("token")?.value;
+
+  try {
+    if (!token) {
+      throw new Error("Token inválido");
+    }
+
+    const { url } = PHOTO_DELETE(id);
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Erro ao deletar a foto");
+    }
+  } catch (error: unknown) {
+    return apiError(error);
+  }
+  revalidateTag("photos");
+  redirect("/conta");
+}
